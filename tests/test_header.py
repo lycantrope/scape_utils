74c17cb44a940b57739e5ffee7cb6485702146c3
Chung-Kuan Chen <b97b01045@gmail.com>
2024-07-12 17:51:33 +0900

    Fix bugs in tests

--- tests/test_header.py
+++ tests/test_header.py
@@ -33,38 +33,38 @@ def file():
     return sample_path
 
 
-def testing_parser(file):
+def testing_parser(file: Path):
     with ScapeVirtualStack(file) as stack:
         v1 = stack.get_volume_raw(0)
         v2 = stack.get_volume_raw(1)
         np.testing.assert_equal(v1, v2)
 
 
-def testing_not_exist(file):
+def testing_not_exist(file: Path):
     data_path = file.with_name("not_exists.3du16")
     with pytest.raises(FileNotFoundError):
         print(ScapeImageDecoder.from_3DU16(data_path))
 
 
-def test_readfile(file):
+def test_readfile(file: Path):
     test_out = file.parent.joinpath("tmp")
     test_out.mkdir(exist_ok=True)
-    with ScapeVirtualStack(SAMPLE_PATH) as stack, pytest.warns(UserWarning):
+    with ScapeVirtualStack(file) as stack, pytest.warns(UserWarning):
         for i in range(4):
             for fmt in ("org", "u8", "f32"):
-                out = test_out.joinpath(SAMPLE_PATH.stem + f"_t={i:0>5d}_{fmt}.tif")
+                out = test_out.joinpath(file.stem + f"_t={i:0>5d}_{fmt}.tif")
                 stack.save_volume_to_tiff(out, i, conversion=fmt)
 
 
-def test_read_volume_fail(file):
+def test_read_volume_fail(file: Path):
     test_out = file.parent.joinpath("tmp")
     test_out.mkdir(exist_ok=True)
     with pytest.raises(IndexError):
-        with ScapeVirtualStack(SAMPLE_PATH) as stack:
+        with ScapeVirtualStack(file) as stack:
             stack.get_volume_raw(100)
 
 
-def test_read_volume_as_imagej(file):
+def test_read_volume_as_imagej(file: Path):
     with ScapeVirtualStack(file) as stack:
         # This method should return 1 volume of image stack with format (1, Z, C, Y, X)
         img = stack.get_imagej_volume(3)
@@ -76,5 +76,5 @@ def test_save_all_volume(file: Path):
     test_out = file.parent.joinpath("tmp")
     test_out.mkdir(exist_ok=True)
     out = test_out.joinpath(file.stem + ".tif")
-    with ScapeVirtualStack(SAMPLE_PATH) as stack:
+    with ScapeVirtualStack(file) as stack:
         stack.save_all_volume_to_tiff(out)
